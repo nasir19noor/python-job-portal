69194b6a1e78068e696374bd6d91210327a5261b
nasir19noor <nasir@nasir.id>
2024-10-08 12:11:47 +0700

    add location db

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 
 #Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'localhost', 'postgresql://postgres:Love1981@localhost/jobportal'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Love1981@localhost/jobportal'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
